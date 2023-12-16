664b9b9c67234c9efab8b5292fa0b060b3df34c3
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2023-12-16 13:34:17 -0600

    feat: pass common-character-count test

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount( s1, s2) {
+  let count = 0;
+  const s1Counts = {};
+  const s2Counts = {};
+
+  // Count the frequency of each character in s1
+  for (let char of s1) {
+    s1Counts[char] = (s1Counts[char] || 0) + 1;
+  }
+
+  // Count the frequency of each character in s2
+  for (let char of s2) {
+    s2Counts[char] = (s2Counts[char] || 0) + 1;
+  }
+
+  // Calculate the total number of common characters
+  for (let char in s1Counts) {
+    if (s2Counts[char]) {
+      count += Math.min(s1Counts[char], s2Counts[char]);
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
