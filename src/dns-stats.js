92d826938d97c3bc04b3cdc2ef829bb916465270
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2023-12-17 19:53:10 -0600

    feat: pass dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains ) {
+  const counts = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let current = '';
+
+    for (const part of parts) {
+      current += `.${part}`;
+      if (!counts[current]) {
+        counts[current] = 0;
+      }
+      counts[current]++;
+    }
+  }
+
+  return counts;
 }
 
 module.exports = {
