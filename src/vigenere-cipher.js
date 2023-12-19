133421d1f63824e27e7c6e2e5119624886082d53
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2023-12-19 08:30:10 -0600

    feat: pass vigenere cipher, what season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,72 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(direct = true) {
+        this.direct = direct;
+    }
+
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw new Error('Incorrect arguments!');
+        }
+        return this.process(message, key, 'encrypt');
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) {
+            throw new Error('Incorrect arguments!');
+        }
+        return this.process(encryptedMessage, key, 'decrypt');
+    }
+
+    process(text, key, mode) {
+        const keyLength = key.length;
+        const aCode = 'A'.charCodeAt(0);
+        const alphabetSize = 26;
+
+        let processedText = '';
+        let keyIndex = 0;
+
+        for (const char of text.toUpperCase()) {
+            if (char >= 'A' && char <= 'Z') {
+                const shift = key[keyIndex % keyLength].toUpperCase().charCodeAt(0) - aCode;
+                const charCode = char.charCodeAt(0) - aCode;
+
+                if (mode === 'encrypt') {
+                    processedText += String.fromCharCode(aCode + (charCode + shift) % alphabetSize);
+                } else { // decrypt mode
+                    processedText += String.fromCharCode(aCode + (charCode - shift + alphabetSize) % alphabetSize);
+                }
+                keyIndex++;
+            } else {
+                processedText += char;
+            }
+        }
+
+        return this.direct ? processedText : processedText.split('').reverse().join('');
+
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
